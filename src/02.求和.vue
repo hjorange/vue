059479fb073acaa92462eb294930e86784ed8627
hu <348968803@qq.com>
2021-08-21 15:27:19 +0800

    vue第三天作业

---
+++ src/02.求和.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+<span v-for="(obj,index) in arr" :key="index"><input type="checkbox" v-model="list" :value="obj">{{obj}}</span>
+
+<p>你选中得元素，累计得值和为：{{sum}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list:[],
+arr:[9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87]
+    }
+  },
+
+  computed: {
+sum(){
+return this.list.reduce((num,obj)=>{
+  return num+=obj
+},0)
+}
+  },
+
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <span>姓名:</span>
+      <input type="text" v-model.trim="name" />
+    </div>
+    <div>
+      <span>年龄:</span>
+      <input type="number" v-model.number.trim="age"/>
+    </div>
+    <div>
+      <span>性别:</span>
+      <select v-model.trim="sex">
+        <option value="男">男</option>
+        <option value="女">女</option>
+      </select>
+    </div>
+    <div>
+      <button @click="btn">添加/修改</button>
+    </div>
+    <div>
+      <table
+        border="1"
+        cellpadding="10"
+        cellspacing="0"
+      >
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+          <th>操作</th>
+        </tr>
+        <tr v-for="(obj,index) in arr" :key="index">
+          <td>{{obj.id}}</td>
+          <td>{{obj.name}}</td>
+          <td>{{obj.age}}</td>
+          <td>{{obj.sex}}</td>
+          <td>
+            <button @click="del(index)">删除</button>
+            <button @click="bj(obj)">编辑</button>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      name:'',
+      age:'',
+      sex:'',
+      is:true,
+arr:[]
+    }
+  },
+
+  methods: {
+btn(){
+if(this.is){
+   const id=this.arr.length>0?this.arr[this.arr.length-1].id+1:1
+  !this.name||!this.age||!this.age?alert('请输入内容'):this.arr.push({
+    id:id,
+    name:this.name,
+    age:this.age,
+    sex:this.sex
+})
+ }
+},
+bj(index){
+  this.is=!this.is
+  console.log(index);
+
+},
+del(index){
+this.arr.splice(index,1)
+}
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
